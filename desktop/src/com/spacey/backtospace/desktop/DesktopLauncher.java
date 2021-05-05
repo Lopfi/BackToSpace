15f52c062159a42d03230215e0893048b1b7db51
Lopfi <robin@dorsti.de>
2021-05-05 19:13:00 +0200

    add simple game code from libgdx for testing

--- desktop/src/com/spacey/backtospace/desktop/DesktopLauncher.java
+++ desktop/src/com/spacey/backtospace/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import com.spacey.backtospace.Game;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Back To Space";
+		config.width = 800;
+		config.height = 480;
 		new LwjglApplication(new Game(), config);
 	}
 }

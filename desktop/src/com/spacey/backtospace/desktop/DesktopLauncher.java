1fa3f4bf2e65134e518ce3837e7c8b204cc4b3a4
Lopfi <robin@dorsti.de>
2021-05-05 18:40:38 +0200

    initialize a new libgdx project

---
+++ desktop/src/com/spacey/backtospace/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.spacey.backtospace.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.spacey.backtospace.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}

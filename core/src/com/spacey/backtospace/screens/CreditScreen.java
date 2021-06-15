df8225407c6bc84d1dd29b841402d65c7275c9de
GidHuper <colin.weyh@web.de>
2021-06-15 18:29:11 +0200

    added icon & darker info/helpscreen

--- core/src/com/spacey/backtospace/screens/CreditScreen.java
+++ core/src/com/spacey/backtospace/screens/CreditScreen.java
@@ -33,7 +33,8 @@ public class CreditScreen extends ScreenAdapter {
 
     @Override
     public void render(float delta) {
-        Gdx.gl.glClearColor(1, .4f, .2f, 1);
+        Gdx.gl.glClearColor(.7f, .2f, .02f, 1);
+        //OLD: Gdx.gl.glClearColor(1, .4f, .2f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         game.batch.begin();

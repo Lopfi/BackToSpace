12dc9e0032e2b1f57de889a90b0621ed80fe701f
GidHuper <colin.weyh@web.de>
2021-06-17 21:03:32 +0200

    fix itembar (itemsize still false)

--- core/src/com/spacey/backtospace/Entity/Inventory.java
+++ core/src/com/spacey/backtospace/Entity/Inventory.java
@@ -10,16 +10,20 @@ public class Inventory extends Entity{
 
     public Item[] items;
     public int itemCount;
+    public int invslots;
+    float scale;
 
     public Inventory(int slots, GameClass game) {
         super();
+        invslots = slots;
+        scale = game.uiScale;
         items = new Item[slots];
         itemCount = 0;
         texture = game.assets.manager.get("player/Itembar.png", Texture.class);
-        width = texture.getWidth() * game.uiScale;
-        height = texture.getHeight() * game.uiScale;
-        pos.x = Gdx.graphics.getWidth() / 2f - width / 2f;
-        pos.y = 40;
+        width = texture.getWidth() * scale;
+        height = texture.getHeight() * scale;
+        pos.x = (Gdx.graphics.getWidth() / 2)- (width/2);
+        pos.y = 15;
     }
 
     public void addItem(Item item) {
@@ -51,9 +55,9 @@ public class Inventory extends Entity{
     @Override
     public void draw(SpriteBatch batch) {
         super.draw(batch);
-        for (Item item : items) {
-            if (item != null) {
-                item.draw(batch);
+        for (int i = 0; i < items.length; i++) {
+            if (items[i] != null) {
+                items[i].draw(batch, i, width, invslots, scale, pos.y);
             }
         }
     }
--- core/src/com/spacey/backtospace/Entity/Item.java
+++ core/src/com/spacey/backtospace/Entity/Item.java
@@ -1,7 +1,9 @@
 package com.spacey.backtospace.Entity;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
 import com.spacey.backtospace.GameClass;
 import com.spacey.backtospace.Helper.Enums;
 
@@ -15,10 +17,23 @@ public class Item extends Entity{
         switch (type) {
             case FUEL:
                 //texture = new Texture("items/Fuel.png");
+                break;
             case WOOD:
                 texture = manager.get("items/Wood.png", Texture.class);
+                break;
             case STONE:
                 texture = manager.get("items/Stone.png", Texture.class);
+                break;
+            case ENGINE:
+                break;
+            case HAMMER:
+                break;
+            default:
+                break;
         }
+    }
+    public void draw(Batch batch, int slot, float width, int invslots, float scale, float y){
+        Float middle = (Gdx.graphics.getWidth() / 2) - ((width/2)) + (width/invslots*slot) - (4*slot);
+        batch.draw(texture, middle, y, texture.getWidth() / scale, texture.getHeight() / scale);
     }
 }

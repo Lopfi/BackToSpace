f1ed6c523477d6f26fbc5baf354bfbdf22881277
RobinD95 <lopfi95@gmail.com>
2021-06-30 00:19:49 +0200

    add buttons to title screen (dont work yet)

--- core/src/com/spacey/backtospace/screens/TitleScreen.java
+++ core/src/com/spacey/backtospace/screens/TitleScreen.java
@@ -6,15 +6,31 @@ import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Matrix4;
+import com.spacey.backtospace.Entity.UI.Button;
 import com.spacey.backtospace.GameClass;
+import com.spacey.backtospace.Helper.Control;
 
 public class TitleScreen extends ScreenAdapter {
 
     GameClass game;
-    //Storytasks its basicly the mission you have to do! YAY :)
+    Control control;
+    SpriteBatch batch;
+
+    private final Matrix4 screenMatrix;
+
+    Button startBtn;
+    Button tutorialBtn;
+    Button settingsBtn;
+    Button shopBtn;
+    Button creditsBtn;
+    Button quitBtn;
+
+    //Story tasks its basically the mission you have to do! YAY :)
     public static String[] Tasks = {
         "Please remove your Data because you start at level 1",
-        "Find the main body of your rocket and put it up", // für jeden schritt wird eine Schraube und der Schraubenzieher benötigt
+        "Find the main body of your rocket and put it up", // für jeden schritt wird eine Schraube und der Schraubenzieher benötigt der wird aber nicht verbraucht
         "Find and use a screwdriver with 1x screw and 1x body plate to repair the main body",
         "Find and attach the lower left fin with 1x screw to your rocket",
         "Find and attach 1x body plate with 1x screw to your rocket",
@@ -23,9 +39,23 @@ public class TitleScreen extends ScreenAdapter {
         "Use the key to enter and start your rocket",
         "Say Goodbye and get back home, Safely."
     };
+
     public TitleScreen(GameClass game) {
         this.game = game;
         game.isPaused = false;
+        batch = game.batch;
+
+        screenMatrix = new Matrix4(batch.getProjectionMatrix().setToOrtho2D(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight()));
+
+
+        control = new Control(Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), game.camera);
+
+        startBtn = new Button(game, control, game.assets.manager.get("ui/buttons/start.png", Texture.class), true, 100, 420);
+        tutorialBtn = new Button(game, control, game.assets.manager.get("ui/buttons/help.png", Texture.class), true, 100, 340);
+        settingsBtn = new Button(game, control, game.assets.manager.get("ui/buttons/settings.png", Texture.class), true, 100, 260);
+        creditsBtn = new Button(game, control, game.assets.manager.get("ui/buttons/credits.png", Texture.class), true, 100, 180);
+        quitBtn = new Button(game, control, game.assets.manager.get("ui/buttons/quit.png", Texture.class), true, 100, 100);
+        shopBtn = new Button(game, control, game.assets.manager.get("ui/buttons/shop.png", Texture.class), true, 1200, 20);
     }
 
     @Override
@@ -65,20 +95,43 @@ public class TitleScreen extends ScreenAdapter {
     public void render(float delta) {
         Gdx.gl.glClearColor(0, .25f, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        game.batch.begin();
-        game.batch.draw(game.assets.manager.get("screens/background.png", Texture.class), 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-        game.batch.draw(game.assets.manager.get("screens/backtospace.png", Texture.class), Gdx.graphics.getWidth() * .3f, Gdx.graphics.getHeight() * .77f, 400, 140);
-        //game.font.getData().setScale(2);
-        //game.font.draw(game.batch, "__ BACK TO SPACE __", Gdx.graphics.getWidth() * .3f, Gdx.graphics.getHeight() * .83f);
-        //game.font.getData().setScale(1);
-        game.font.draw(game.batch, "Level: ["+game.safe.level+"]     Coins: ["+game.safe.coins+"]", Gdx.graphics.getWidth() * .3f, Gdx.graphics.getHeight() * .71f);
-        game.font.draw(game.batch, "Task:  " + Tasks[game.safe.level], Gdx.graphics.getWidth() * .3f, Gdx.graphics.getHeight() * .65f);
-        game.font.draw(game.batch, "[H] Tutorial/Help", Gdx.graphics.getWidth() * .3f, Gdx.graphics.getHeight() * .36f);
-        game.font.draw(game.batch, "[D] Design/Shop", Gdx.graphics.getWidth() * .3f, Gdx.graphics.getHeight() * .32f);
-        game.font.draw(game.batch, "[S] Settings", Gdx.graphics.getWidth() * .3f, Gdx.graphics.getHeight() * .44f);
-        game.font.draw(game.batch, "[C] Credits", Gdx.graphics.getWidth() * .3f, Gdx.graphics.getHeight() * .4f);
-        game.font.draw(game.batch, "->  [SPACE] PLAY  <-", Gdx.graphics.getWidth() * .3f, Gdx.graphics.getHeight() * .25f);
-        game.batch.end();
+
+        startBtn.update();
+        tutorialBtn.update();
+        settingsBtn.update();
+        shopBtn.update();
+        creditsBtn.update();
+        quitBtn.update();
+
+        if (startBtn.pressed) Gdx.app.log("Button", "start");
+        if (tutorialBtn.pressed)
+        if (settingsBtn.pressed)
+        if (shopBtn.pressed)
+        if (creditsBtn.pressed)
+        if (quitBtn.pressed){
+
+        }
+
+        batch.begin();
+        batch.draw(game.assets.manager.get("screens/background.png", Texture.class), 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        batch.draw(game.assets.manager.get("screens/backtospace.png", Texture.class), Gdx.graphics.getWidth() * .3f, Gdx.graphics.getHeight() * .77f, 400, 140);
+        game.font.draw(batch, "Level: ["+game.safe.level+"]     Coins: ["+game.safe.coins+"]", Gdx.graphics.getWidth() * .3f, Gdx.graphics.getHeight() * .71f);
+        game.font.draw(batch, "Task:  " + Tasks[game.safe.level], Gdx.graphics.getWidth() * .3f, Gdx.graphics.getHeight() * .65f);
+        game.font.draw(batch, "[H] Tutorial/Help", Gdx.graphics.getWidth() * .3f, Gdx.graphics.getHeight() * .36f);
+        game.font.draw(batch, "[D] Design/Shop", Gdx.graphics.getWidth() * .3f, Gdx.graphics.getHeight() * .32f);
+        game.font.draw(batch, "[S] Settings", Gdx.graphics.getWidth() * .3f, Gdx.graphics.getHeight() * .44f);
+        game.font.draw(batch, "[C] Credits", Gdx.graphics.getWidth() * .3f, Gdx.graphics.getHeight() * .4f);
+        game.font.draw(batch, "->  [SPACE] PLAY  <-", Gdx.graphics.getWidth() * .3f, Gdx.graphics.getHeight() * .25f);
+
+        batch.setProjectionMatrix(screenMatrix);
+        startBtn.draw(batch);
+        tutorialBtn.draw(batch);
+        settingsBtn.draw(batch);
+        shopBtn.draw(batch);
+        creditsBtn.draw(batch);
+        quitBtn.draw(batch);
+
+        batch.end();
     }
 
     @Override

e3355cb89977337d345545f5a18e8f44b277ddb0
GidHuper <colin.weyh@web.de>
2021-07-01 17:40:25 +0200

    better settingsscreen?

--- core/src/com/spacey/backtospace/screens/SettingsScreen.java
+++ core/src/com/spacey/backtospace/screens/SettingsScreen.java
@@ -6,7 +6,11 @@ import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Matrix4;
 import com.spacey.backtospace.GameClass;
+import com.spacey.backtospace.Entity.UI.Button;
 import com.spacey.backtospace.Helper.Control;
 import com.spacey.backtospace.Helper.DataSafe;
 
@@ -16,11 +20,26 @@ public class SettingsScreen extends ScreenAdapter {
     public Boolean deleteMode; //if u want to remove all the data
     final float firstLineY;
     public long SoundId;
+    float textX = Gdx.graphics.getWidth() * .25f;
+
+    Control control;
+    SpriteBatch batch;
+    private final Matrix4 screenMatrix;
+    
+    Button home;
+    Button play;
 
     public SettingsScreen(GameClass game) {
         this.game = game;
         deleteMode = false;
-        firstLineY = Gdx.graphics.getHeight() * .80f;
+        firstLineY = Gdx.graphics.getHeight() * .88f;
+
+        batch = game.batch;
+        screenMatrix = new Matrix4(batch.getProjectionMatrix().setToOrtho2D(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight()));
+        control = new Control(Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), game.camera);
+
+        home = new Button(game, control, game.assets.manager.get("ui/buttons/home.png", Texture.class), true, textX*2, Gdx.graphics.getHeight() * .10f);
+        play = new Button(game, control, game.assets.manager.get("ui/buttons/start.png", Texture.class), true, textX/2, Gdx.graphics.getHeight() * .10f);
     }
 
     @Override
@@ -112,19 +131,19 @@ public class SettingsScreen extends ScreenAdapter {
 
     @Override
     public void render(float delta) {
-        Gdx.gl.glClearColor(1, .4f, .2f, 1);
+        Gdx.gl.glClearColor(.5f, .6f, .44f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        float textX = Gdx.graphics.getWidth() * .25f;
-
-        game.batch.begin();
-
-        game.font.draw(game.batch, "SETTINGS", textX, getLineY(0));
+        batch.begin();
+        batch.draw(game.assets.manager.get("screens/gears.png", Texture.class), Gdx.graphics.getWidth() -200, Gdx.graphics.getHeight() -200, 150, 150);
+        game.font.getData().setScale(2);
+        game.font.draw(batch, "SETTINGS:", textX, getLineY(0));
+        game.font.getData().setScale(1);
         if (deleteMode){
-            game.font.draw(game.batch, "!! WARNING !!", textX, getLineY(2));
-            game.font.draw(game.batch, "Are you sure you want to delete all game date?", textX, getLineY(3));
-            game.font.draw(game.batch, "[N] NO", textX, getLineY(5));
-            game.font.draw(game.batch, "[Y] YES DELETE EVERYTHING AND CLOSE", textX, getLineY(6));
+            game.font.draw(batch, "!! WARNING !!", textX, getLineY(2));
+            game.font.draw(batch, "Are you sure you want to delete all game date?", textX, getLineY(3));
+            game.font.draw(batch, "[N] NO", textX, getLineY(5));
+            game.font.draw(batch, "[Y] YES DELETE EVERYTHING AND CLOSE", textX, getLineY(6));
         } else if (musicMode){
         if(game.assets.manager.update()) {
             //music play logic
@@ -144,20 +163,35 @@ public class SettingsScreen extends ScreenAdapter {
                 game.introSound.setVolume(SoundId,game.safe.playVolume);
             }
         }
-        game.font.draw(game.batch, "LOADING MUSIC FILES..", textX, getLineY(3));
-        game.font.draw(game.batch, "Please wait a second (" + Math.round(game.assets.manager.getProgress()*100) + "%)", textX, getLineY(5));
+        game.font.draw(batch, "LOADING MUSIC FILES..", textX, getLineY(3));
+        game.font.draw(batch, "Please wait a second (" + Math.round(game.assets.manager.getProgress()*100) + "%)", textX, getLineY(5));
         }
         else {// TODO: ADD BUTTON SUPPORT HERE @robin i can do that if u want
-        game.font.draw(game.batch, "Music:        <" + game.safe.playMusic + "> [M] to toggle", textX, Gdx.graphics.getHeight() * .7f);
-        game.font.draw(game.batch, "Volume:     <" + Math.round(game.safe.playVolume*10) + "> [UP][DOWN] to change", textX, Gdx.graphics.getHeight() * .66f);
-        game.font.draw(game.batch, "GameTask: <" + game.safe.showTask + "> [T] to toggle", textX, Gdx.graphics.getHeight() * .62f);
-        game.font.draw(game.batch, "Press [C] to play the Cutszene again", textX, Gdx.graphics.getHeight() * .58f);
-        if (Control.debug) game.font.draw(game.batch, "Money:    <" + game.safe.coins + "> [A][O] All or zerO", textX, Gdx.graphics.getHeight() * .45f);
-        game.font.draw(game.batch, "[R] Reset game data", textX, Gdx.graphics.getHeight() * .35f);
-        game.font.draw(game.batch, "[SPACE/ESC] Play Game", textX, Gdx.graphics.getHeight() * .28f);
-        game.font.draw(game.batch, "[ENTER] Main Menu", textX, Gdx.graphics.getHeight() * .25f);
+        
+            play.update();
+            home.update();
+    
+            if (home.pressed) game.setScreen(game.gameScreen);
+            if (play.pressed) game.setScreen(new TitleScreen(game));
+         game.font.draw(batch, "Music:        <" + game.safe.playMusic + "> [M] to toggle", textX, Gdx.graphics.getHeight() * .7f);
+        game.font.draw(batch, "Volume:     <" + Math.round(game.safe.playVolume*10) + "> [UP][DOWN] to change", textX, Gdx.graphics.getHeight() * .66f);
+        game.font.draw(batch, "GameTask: <" + game.safe.showTask + "> [T] to toggle", textX, Gdx.graphics.getHeight() * .62f);
+        game.font.draw(batch, "Press [C] to play the Cutszene again", textX, Gdx.graphics.getHeight() * .38f);
+        if (Control.debug) game.font.draw(batch, "Money:    <" + game.safe.coins + "> [A][O] All or zerO", textX, Gdx.graphics.getHeight() * .45f);
+        game.font.draw(batch, "[R] Reset game data", textX, Gdx.graphics.getHeight() * .35f);
+        game.font.draw(batch, "[SPACE/ESC]", textX/2, Gdx.graphics.getHeight() * .07f);
+        game.font.draw(batch, "[ENTER]", textX*2, Gdx.graphics.getHeight() * .07f);
+        
+        if (control.Space) {
+            game.setScreen(game.gameScreen);
+        } else if (control.keyDown(66)) {
+            game.setScreen(new TitleScreen(game));
         }
-        game.batch.end();
+        batch.setProjectionMatrix(screenMatrix);
+        play.draw(batch);
+        home.draw(batch);    
+    }
+        batch.end();
 
     }
 
